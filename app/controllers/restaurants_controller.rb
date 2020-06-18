448f389b4bdbab8754a59742999b27e47e3b1fbb
Juan Ceschin <juanceschin@gmail.com>
2020-06-17 22:59:28 -0300

    Embeding review form inside restaurant's show view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -7,6 +7,7 @@ class RestaurantsController < ApplicationController
   # List one restaurant (show)
   def show
     @restaurant = Restaurant.find(params[:id])
+    @review = Review.new
   end
 
   # Create new restaurant (new and create)
@@ -18,11 +19,20 @@ class RestaurantsController < ApplicationController
     @restaurant = Restaurant.new(restaurant_params)
     @restaurant.save
     redirect_to restaurants_path
+
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+    @review.save
+    redirect_to restaurants_path(@restaurant)
   end
 
   private
 
   def restaurant_params
     params.require(:restaurant).permit(:name, :address, :phone_number, :category)
+  end
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,21 +1,21 @@
 class ReviewsController < ApplicationController
   # New review
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
+  # def new
+  # @restaurant = Restaurant.find(params[:restaurant_id])
+  # @review = Review.new
+  # end
 
-  def create
-    @review = Review.new(review_params)
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review.restaurant = @restaurant
-    @review.save
-    redirect_to restaurants_path
-  end
+  # def create
+  # @review = Review.new(review_params)
+  # @restaurant = Restaurant.find(params[:restaurant_id])
+  # @review.restaurant = @restaurant
+  # @review.save
+  # redirect_to restaurants_path
+  # end
 
-  private
+  # private
 
-  def review_params
-    params.require(:review).permit(:content, :rating)
-  end
+  # def review_params
+  # params.require(:review).permit(:content, :rating)
+  # end
 end
